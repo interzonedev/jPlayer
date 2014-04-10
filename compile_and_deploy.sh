d37516d376cf1f2f817837ea2c44709eea127a54
Mark Markarian <mark@interzoendev.com>
2014-04-09 17:00:28 -0700

    Replaced the compile and deploy script with an Ant build file to compile and deploy the Jplayer.swf.

--- compile_and_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "compiling"
-
-mxmlc -output=jquery.jplayer/Jplayer.swf actionscript/Jplayer.as
-
-echo "deploying"
-
-cp jquery.jplayer/Jplayer.swf $PROJECTS_DIR/radio/www/js/libs/jquery.jplayer-2.3.9/
